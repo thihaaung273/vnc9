1ea493a078f76aaa39a16e94e18dced218a67bfc
thetmuthakhin <bqv@ucupbae.ml>
2017-01-17 23:44:17 +0630

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VIDEOS=~/Desktop/convert/ 
+VIDEOS=~/Downloads/ 
 find "$VIDEOS" -name '*.mp4' -exec sh -c 'rar a -pthetmuthakhin -v260000 "${0%%.mp4}.rar" "$0"' {} \; 
 find "$VIDEOS" -name '*.rar' -exec sh -c 'cat vps.gif "$0" > "${0%%.rar}.gif"' {} \; 
 find "$VIDEOS" -name '*.mp4' -exec sh -c 'rm Desktop/convert/*.mp4' {} \; 
