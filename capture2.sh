8b6838d210e446c529d67fa882bfb149c1a2bfa5
thetmuthakhin <bqv@ucupbae.ml>
2017-01-18 00:30:09 +0630

    Update capture2.sh

--- capture2.sh
+++ capture2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VIDEOS=~/Downloads/ 
-find "$VIDEOS" -name '*.mp4' -exec sh -c 'MP4Box -split 260000 "$0"' {} \; 
+find "$VIDEOS" -name '*.mp4' -exec sh -c 'MP4Box -splits 260000 "$0"' {} \; 
 
 exit;
 
