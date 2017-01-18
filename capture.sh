7f0c580d56144cafd5ed601e0957c18faa4b2345
thetmuthakhin <bqv@ucupbae.ml>
2017-01-18 21:13:22 +0630

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VIDEOS=~/Downloads/ 
-find "$VIDEOS" -name '*.mp4' -exec sh -c 'rar a -pthetmuthakhin -v260000 "${0%%.mp4}.rar" "$0"' {} \; 
+find "$VIDEOS" -name '*.mp4' -exec sh -c 'rar a -pthetmuthakhin -v70000 "${0%%.mp4}.rar" "$0"' {} \; 
 find "$VIDEOS" -name '*.rar' -exec sh -c 'cat vps.gif "$0" > "${0%%.rar}.gif"' {} \; 
 find "$VIDEOS" -name '*.mp4' -exec sh -c 'rm Downloads/*.mp4' {} \; 
 find "$VIDEOS" -name '*.rar' -exec sh -c 'rm Downloads/*.rar' {} \; 
